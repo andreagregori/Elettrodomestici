291c13ba85359825966e511ef173743d5d35433f
andreagregori <32836744+andreagregori@users.noreply.github.com>
2020-01-12 22:32:15 +0100

    Prime prove con i file
    Ho capito come funzionano i file 👍  (leggo le righe ma devo trovare il modo di separare le varie informazioni)

--- Sorgente1.cpp
+++ Sorgente1.cpp
@@ -1,1 +1,28 @@
 //Andrea Gregori
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Intestazione.h"
+
+using namespace std;
+
+
+
+//Funzione che legge il file components_info.dat e carica i vari componenti in comp_disponibili
+void Gestione::inizializza_comp() {
+	ifstream components("components_info.dat");
+	if (components.is_open())
+	{
+		string line;
+		while (getline(components, line))
+		{
+			cout << line;
+		}
+	}
+}
+
+//Funzione che legge il file models.dat (e i vari model1.dat..) e carica i vari modelli in elet_disponibili
+void Gestione::inizializza_elet() {
+
+}
